ce42d7da464f15e1c71d7092a2518eef78d0be47
IgorSondors <igor.sondors@gmail.com>
2024-01-18 14:47:01 +0000

    one ckpt eval

--- EVAL_by_category.py
+++ EVAL_by_category.py
@@ -117,10 +117,10 @@ def wrt_json(categories, pth_models, pth_offers, ckpt_pth, tmp_fld, pth_dst_json
         txt.write(f"Save pth: {pth_dst_json}_{ckpt_pth.split('/')[-1]}.json\n")
 
 if __name__=='__main__':
-    pth_models = "/home/sondors/Documents/price/ColBERT_data/18_categories/test/models_18_categories.csv"
-    pth_offers = "/home/sondors/Documents/price/ColBERT_data/18_categories/test/triplets_test_18_categories.csv"
-    tmp_fld = "/home/sondors/Documents/price/ColBERT/tmp"
-    pth_dst_json = "/home/sondors/Documents/price/ColBERT/tmp/triples_X3"
+    pth_models = "/mnt/vdb1/Datasets/ColBERT/18_categories/test/models_18_categories.csv"
+    pth_offers = "/mnt/vdb1/Datasets/ColBERT/18_categories/test/triplets_test_18_categories.csv"
+    tmp_fld = "/mnt/vdb1/ColBERT/tmp"
+    pth_dst_json = "/mnt/vdb1/ColBERT/tmp/triples_accum_12"
 
     categories = [
         "диктофоны, портативные рекордеры",
@@ -139,13 +139,13 @@ if __name__=='__main__':
         "GPS-навигаторы"
         ]
     
-    ckpts_pth = "/home/sondors/HYPERPARAM/none/2024-01/09/22.18.23/checkpoints"
-    for checkpoint in os.listdir(ckpts_pth):
-        ckpt_pth = os.path.join(ckpts_pth, checkpoint)
-        
-        all_categories_time = time.time()
-        wrt_json(categories, pth_models, pth_offers, ckpt_pth, tmp_fld, pth_dst_json)
-        with open(f"{tmp_fld}/logs.txt", "a") as txt:
-            txt.write(f"all_categories_time = {time.time() - all_categories_time}\n\n")
-            txt.write("-"*100)
+    # ckpts_pth = "/home/sondors/HYPERPARAM/none/2024-01/09/22.18.23/checkpoints"
+    # for checkpoint in os.listdir(ckpts_pth):
+    #     ckpt_pth = os.path.join(ckpts_pth, checkpoint)
+    ckpt_pth = "/mnt/vdb1/ColBERT/experiments/HYPERPARAM_accum_12/none/2024-01/16/13.07.51/checkpoints/colbert-6000-finish"
+    all_categories_time = time.time()
+    wrt_json(categories, pth_models, pth_offers, ckpt_pth, tmp_fld, pth_dst_json)
+    with open(f"{tmp_fld}/logs.txt", "a") as txt:
+        txt.write(f"all_categories_time = {time.time() - all_categories_time}\n\n")
+        txt.write("-"*100)
 
