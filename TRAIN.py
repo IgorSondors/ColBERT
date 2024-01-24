aaa67406d97fb390e7c259bb219c10d8026e16fd
IgorSondors <igor.sondors@gmail.com>
2024-01-24 19:18:46 +0000

    13 categories aug train

--- TRAIN.py
+++ TRAIN.py
@@ -20,9 +20,9 @@ model_name: str = DefaultVal(None) # DefaultVal('bert-base-uncased')
 
 checkpoint = 'colbert-ir/colbertv2.0'
 if __name__=='__main__':
-    triples="/mnt/vdb1/Datasets/ColBERT_data/13_categories/train/triples_X1_13_categories_shuffle.json"
-    queries="/mnt/vdb1/Datasets/ColBERT_data/13_categories/train/queries_train_13_categories.tsv"
-    collection="/mnt/vdb1/Datasets/ColBERT_data/13_categories/train/documents_train_13_categories.tsv"
+    triples="/mnt/vdb1/Datasets/ColBERT_data/13_categories/train_aug/triples_X1_13_categories_aug_shuffle.json"
+    queries="/mnt/vdb1/Datasets/ColBERT_data/13_categories/train_aug/queries_train_13_categories_aug.tsv"
+    collection="/mnt/vdb1/Datasets/ColBERT_data/13_categories/train_aug/documents_train_13_categories_aug.tsv"
     # DocSettings:
     doc_maxlen=180
     dim=128
@@ -34,13 +34,13 @@ if __name__=='__main__':
 
     nway=2                                      # https://github.com/stanford-futuredata/ColBERT/issues/245
     lr=1e-05
-    bsize=1#128*4
+    bsize=128#128*4
     accumsteps=1                                # на сколько элементов из батча аккумулировать лосс
     n_triplets = sum(1 for _ in open(triples))  # количество строк в triples.json
     steps_per_epoch = int(n_triplets/bsize)     # количество батчей в эпохе. ColBERT обучается по всем строкам файла один раз без эпох
     warmup=0                                    # через сколько шагов сделать warmup до изначального lr
 
-    with Run().context(RunConfig(nranks=1, experiment="HYPERPARAM_shuffle_13_categories_bsize1")): # nranks - число видеокарт
+    with Run().context(RunConfig(nranks=1, experiment="HYPERPARAM_shuffle_13_categories_aug")): # nranks - число видеокарт
         config = ColBERTConfig(bsize=bsize, 
                                 lr=lr, 
                                 warmup=warmup, 
