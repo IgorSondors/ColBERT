079540db4d48355514885a2a69849a521ad30caf
IgorSondors <igor.sondors@gmail.com>
2024-01-27 17:33:09 +0000

    minor

--- TRAIN.py
+++ TRAIN.py
@@ -21,13 +21,17 @@ model_name: str = DefaultVal(None) # DefaultVal('bert-base-uncased')
 checkpoint = 'bert-base-multilingual-cased'
 # checkpoint = 'colbert-ir/colbertv2.0'
 if __name__=='__main__':
-    triples="/mnt/vdb1/Datasets/ColBERT_data/13_categories/train_nway_6/triples_X1_13_categories_aug_nway_6_shuffle.json"
-    queries="/mnt/vdb1/Datasets/ColBERT_data/13_categories/train_nway_6/queries_train_13_categories_aug_nway_6.tsv"
-    collection="/mnt/vdb1/Datasets/ColBERT_data/13_categories/train_nway_6/documents_train_13_categories_aug_nway_6.tsv"
+    # triples="/mnt/vdb1/Datasets/ColBERT_data/13_categories/train_aug/triples_X1_13_categories_aug_shuffle.json"
+    # queries="/mnt/vdb1/Datasets/ColBERT_data/13_categories/train_aug/queries_train_13_categories_aug.tsv"
+    # collection="/mnt/vdb1/Datasets/ColBERT_data/13_categories/train_aug/documents_train_13_categories_aug.tsv"
 
-    # triples="/mnt/vdb1/Datasets/ColBERT_data/13_categories/train/triples_X1_13_categories_shuffle.json"
-    # queries="/mnt/vdb1/Datasets/ColBERT_data/13_categories/train/queries_train_13_categories.tsv"
-    # collection="/mnt/vdb1/Datasets/ColBERT_data/13_categories/train/documents_train_13_categories.tsv"
+    # triples="/mnt/vdb1/Datasets/ColBERT_data/13_categories/train_nway_6/triples_X1_13_categories_aug_nway_6_shuffle.json"
+    # queries="/mnt/vdb1/Datasets/ColBERT_data/13_categories/train_nway_6/queries_train_13_categories_aug_nway_6.tsv"
+    # collection="/mnt/vdb1/Datasets/ColBERT_data/13_categories/train_nway_6/documents_train_13_categories_aug_nway_6.tsv"
+
+    triples="/mnt/vdb1/Datasets/ColBERT_data/13_categories/train/triples_X1_13_categories_shuffle.json"
+    queries="/mnt/vdb1/Datasets/ColBERT_data/13_categories/train/queries_train_13_categories.tsv"
+    collection="/mnt/vdb1/Datasets/ColBERT_data/13_categories/train/documents_train_13_categories.tsv"
 
     # DocSettings:
     doc_maxlen=180
@@ -38,15 +42,15 @@ if __name__=='__main__':
     save_every = None
     root="/home/sondors/Documents/1234567"      # не работает
 
-    nway=6#2                                    # https://github.com/stanford-futuredata/ColBERT/issues/245
+    nway=2#2                                    # https://github.com/stanford-futuredata/ColBERT/issues/245
     lr=1e-04
-    bsize=32#128#40
+    bsize=230#128#40
     accumsteps=1                                # на сколько элементов из батча аккумулировать лосс
     n_triplets = sum(1 for _ in open(triples))  # количество строк в triples.json
     steps_per_epoch = int(n_triplets/bsize)     # количество батчей в эпохе. ColBERT обучается по всем строкам файла один раз без эпох
     warmup=0                                    # через сколько шагов сделать warmup до изначального lr
 
-    experiment = "triples_X1_13_categories_bert-base-multilingual-cased_dim_768_use_ib_negatives_nway_6"
+    experiment = "bert-base-multilingual-cased_dim_768_bsize_230_lr04_use_ib_negatives"
     with Run().context(RunConfig(nranks=1, experiment=experiment)): # nranks - число видеокарт
         config = ColBERTConfig(bsize=bsize, 
                                 lr=lr, 
