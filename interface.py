bde2b1e4f100b4b2be96663de9b889c5208be1cd
i.sondors <igor.sondors@price-team.ru>
2024-02-07 19:47:34 +0300

    add typing

--- interface.py
+++ interface.py
@@ -1,3 +1,4 @@
+from typing import Tuple, List, Dict, Union, Any
 from colbert.infra import Run, RunConfig, ColBERTConfig
 from colbert.data import Queries, Collection
 from colbert import Indexer, Searcher
@@ -5,7 +6,7 @@ from colbert import Indexer, Searcher
 import pandas as pd
 import os
 
-def prepare_tsv(df, dst_fld, category_id):
+def prepare_tsv(df: pd.DataFrame, dst_fld: str, category_id: str) -> None:
     """
     Prepare and save two TSV files containing model information. Below is default format for input of models to ColBERT to index them.
 
@@ -21,45 +22,43 @@ def prepare_tsv(df, dst_fld, category_id):
     1   model_id1
     2   model_id2
 
-    Parameters:
-        df (DataFrame): DataFrame containing models information.
+    Args:
+        df (pd.DataFrame): DataFrame containing models information.
         dst_fld (str): Destination folder where TSV files will be saved.
         category_id (str): Identifier for the category of models.
 
     Returns:
         None
     """
-
     os.makedirs(os.path.join(dst_fld, "tsv"), exist_ok=True)
     models, models_id = df_split(df)
     models.to_csv(os.path.join(dst_fld, "tsv", f"{category_id}_models.tsv"), sep='\t', header=False, index=False)
     models_id.to_csv(os.path.join(dst_fld, "tsv", f"{category_id}_models_id.tsv"), sep='\t', header=False, index=False)
 
-def df_split(df):
+def df_split(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """
     Split the DataFrame into two separate DataFrames.
 
-    Parameters:
-        df (DataFrame): Input DataFrame containing models information.
+    Args:
+        df (pd.DataFrame): Input DataFrame containing models information.
 
     Returns:
-        DataFrame: DataFrame containing model full names.
-        DataFrame: DataFrame containing model IDs.
+        Tuple[pd.DataFrame, pd.DataFrame]: DataFrame containing model full names and DataFrame containing model IDs.
     """
     df1 = pd.DataFrame({'id': range(len(df)), 'full_name': df["full_name"]})
     df2 = pd.DataFrame({'id': range(len(df)), 'model_id': df["model_id"]})
     return df1, df2
 
-def save_index(ckpt_fld, doc_maxlen, nbits, kmeans_niters, nranks, dst_fld, experiment, collection, index_name):
+def save_index(ckpt_fld: str, doc_maxlen: int, nbits: int, kmeans_niters: int, nranks: int, dst_fld: str, experiment: str, collection: Collection, index_name: str) -> Indexer:
     """
     Index the collection of documents and save the index for fast search of model that matches to given offer.
 
-    Parameters:
+    Args:
         ckpt_fld (str): Path to the checkpoint folder.
         doc_maxlen (int): Maximum length of the document.
         nbits (int): Number of bits for the embedding.
-        kmeans_niters (int): Number of iterations of k-means clustering; 4 is a good and fast default
-        nranks (int): Number of GPUs to use, if they are available
+        kmeans_niters (int): Number of iterations of k-means clustering; 4 is a good and fast default.
+        nranks (int): Number of GPUs to use, if they are available.
         dst_fld (str): Destination folder where the index will be saved.
         experiment (str): Experiment name.
         collection (Collection): Collection of documents to index.
@@ -74,39 +73,16 @@ def save_index(ckpt_fld, doc_maxlen, nbits, kmeans_niters, nranks, dst_fld, expe
         indexer.index(name=index_name, collection=collection, overwrite=True)
     return indexer
 
-def top_n_similar(offers, src_fld, nranks, experiment, index_name, model_ids, n):
-    """
-    Retrieve top N similar models for given offers.
-
-    Parameters:
-        offers (list): List of offers.
-        src_fld (str): Source folder where the index is located.
-        nranks (int): Number of GPUs to use, if they are available
-        experiment (str): Experiment name.
-        index_name (str): Name of the index.
-        model_ids (list): List of model IDs.
-        n (int): Number of similar models to retrieve.
-
-    Returns:
-        list: List of dictionaries containing model IDs and their similarities.
-    """
-    with Run().context(RunConfig(nranks=nranks, root=src_fld, experiment=experiment)):
-        searcher = Searcher(index=index_name, collection=model_ids)
-        offers = Queries(data=offers)
-        rankings = searcher.search_all(offers, k=n)
-        top_n = rankings_to_dict(rankings, searcher)
-    return top_n
-
-def rankings_to_dict(rankings, searcher):
+def rankings_to_dict(rankings: List[Tuple[str, List[Tuple[int, float]]]], searcher: Searcher) -> List[Dict[str, Any]]:
     """
     Convert rankings object to a list of dictionaries.
 
-    Parameters:
-        rankings (Rankings): Rankings object containing search results.
+    Args:
+        rankings (List[Tuple[str, List[Tuple[int, float]]]]): Rankings object containing search results.
         searcher (Searcher): Searcher object used for searching.
 
     Returns:
-        list: List of dictionaries containing model IDs and their similarities.
+        List[Dict[str, Any]]: List of dictionaries containing model IDs and their similarities.
     """
     result = []
     for key, value in rankings.todict().items():
@@ -114,3 +90,26 @@ def rankings_to_dict(rankings, searcher):
         similarity = [item[2] for item in value]
         result.append({'model_ids': model_ids, 'similarity': similarity})
     return result
+
+def top_n_similar(offers: Dict[int, str], src_fld: str, nranks: int, experiment: str, index_name: str, model_ids: List[str], n: int) -> List[Dict[str, Union[List[int], List[float]]]]:
+    """
+    Retrieve top N similar models for given offers.
+
+    Args:
+        offers (Dict[int, str]): Dictionary where keys are integer IDs and values are offer strings.
+        src_fld (str): Source folder where the index is located.
+        nranks (int): Number of GPUs to use, if they are available.
+        experiment (str): Experiment name.
+        index_name (str): Name of the index.
+        model_ids (List[str]): List of model IDs.
+        n (int): Number of similar models to retrieve.
+
+    Returns:
+        List[Dict[str, Union[List[int], List[float]]]]: List of dictionaries containing model IDs and their similarities.
+    """
+    with Run().context(RunConfig(nranks=nranks, root=src_fld, experiment=experiment)):
+        searcher = Searcher(index=index_name, collection=model_ids)
+        offers = Queries(data=offers)
+        rankings = searcher.search_all(offers, k=n)
+        top_n = rankings_to_dict(rankings, searcher)
+    return top_n
