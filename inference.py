6d465f216f056cef031132b178f80731de6d1184
i.sondors <igor.sondors@price-team.ru>
2024-02-29 17:55:18 +0300

    minor

--- inference.py
+++ inference.py
@@ -37,12 +37,12 @@ def convert_columns_to_int(df, num_columns):
 
 if __name__=='__main__':
     # pth_offers = "/home/sondors/Documents/price/BERT_data/data/18_categories/triplets_test_18_categories.csv"
-    pth_offers = "/home/sondors/Documents/price/BERT_data/data/13_categories/triplets_13_categories.csv"
+    pth_offers = "/home/sondors/Documents/price/offers_clusterization/data/14_categories_balanced.csv"
     pth_models = "/home/sondors/Documents/price/ColBERT_data/18_categories/test/models_18_categories.csv"
-    dst_fld = "/home/sondors/Documents/price/ColBERT/proverka_triplets_13_categories"
+    dst_fld = "/home/sondors/Documents/price/ColBERT/accuracies"
 
-    ckpt_pth = "/home/sondors/Documents/ColBERT_weights/triples_X1_13_categories_use_ib_negatives/none/2024-01/26/10.49.44/checkpoints/colbert-5387-finish"
-    experiment = "colbert-5387"
+    ckpt_pth = "/home/sondors/triples_X1_13_categories_filtered_shuffle_use_ib_negatives_lr04/colbert-4446-finish"
+    experiment = "colbert-4446"
 
     doc_maxlen = 300
     nbits = 2   # bits определяет количество битов у каждого измерения в семантическом пространстве во время индексации
