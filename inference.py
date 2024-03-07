2dc3793279ecefde989d79fdab6031939688826d
i.sondors <igor.sondors@price-team.ru>
2024-03-07 17:00:15 +0300

    tensor to device

--- inference.py
+++ inference.py
@@ -38,11 +38,13 @@ def convert_columns_to_int(df, num_columns):
 if __name__=='__main__':
     # pth_offers = "/home/sondors/Documents/price/BERT_data/data/18_categories/triplets_test_18_categories.csv"
     pth_offers = "/home/sondors/Documents/price/offers_clusterization/data/14_categories_balanced.csv"
-    pth_models = "/home/sondors/Documents/price/ColBERT_data/18_categories/test/offers_and_models_18_categories.csv"
-    dst_fld = "/home/sondors/Documents/price/ColBERT/accuracies/offers_and_models"
+    # pth_models = "/home/sondors/Documents/price/ColBERT_data/18_categories/test/offers_and_models_18_categories.csv"
+    pth_models = "/home/sondors/Documents/price/BERT_data/data/models_18_categories.csv"
+    # dst_fld = "/home/sondors/Documents/price/ColBERT/accuracies/offers_and_models"
+    dst_fld = "/home/sondors/Documents/price/ColBERT/triples_X1_13_categories_use_ib_negatives_nway3_lr04"
 
-    ckpt_pth = "/home/sondors/triples_X1_13_categories_filtered_shuffle_use_ib_negatives_lr04/colbert-4446-finish"
-    experiment = "colbert-4446"
+    ckpt_pth = "/home/sondors/triples_X1_13_categories_use_ib_negatives_nway3_lr04/colbert-9555"
+    experiment = "colbert-9555"
 
     doc_maxlen = 300
     nbits = 2   # bits определяет количество битов у каждого измерения в семантическом пространстве во время индексации
